ed23023cdf5d14e5113f36d832182ef12fa2b515
takoyaki-3 <mail@takoyaki3.com>
2024-10-27 19:46:07 +0900

    StoryBookをデプロイ対象から排除

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { copyFileSync } from 'fs';
+// import { resolve } from 'path';
+// import { copyFileSync } from 'fs';
 
 export default defineConfig({
-  base: '/storybook/', // Storybook用のベースパス
   plugins: [
     react(),
     {
       name: 'copy-index-to-404',
-      buildEnd() {
-        // ビルド後に index.html を 404.html にコピー
-        try {
-          copyFileSync(resolve(__dirname, 'dist/index.html'), resolve(__dirname, 'dist/404.html'));
-        } catch (error) {
-          console.error('404.html ファイルのコピーに失敗しました:', error);
-        }
+      closeBundle() {
+        // // ビルド後に index.html を 404.html にコピー
+        // copyFileSync(resolve(__dirname, 'dist/index.html'), resolve(__dirname, 'dist/404.html'));
       },
     },
   ],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 });
